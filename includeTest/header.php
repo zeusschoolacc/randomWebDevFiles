6e8faf6136bdb9b4ffe59f0fc669f72c038231b1
Zeus D. Elderfield <150475099+zeusschoolacc@users.noreply.github.com>
2024-04-11 09:39:55 +0800

    Create header.php

---
+++ includeTest/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>MY PHP</title>
+</head>
+<body>
+ <div style="width: 100%;
+            height: 100px;
+            background-color: green;">
+    <h3>Header</h3>
+ </div>
