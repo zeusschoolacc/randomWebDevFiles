f89c4f5ea199906857f9446608964ce39c5dc1ed
Zeus D. Elderfield <150475099+zeusschoolacc@users.noreply.github.com>
2024-04-11 09:39:33 +0800

    Create index.php

---
+++ includeTest/index.php
@@ -0,0 +1,16 @@
+<?php include "header.php"; ?>
+
+<div style="width: 15%;
+            height: 500px;
+            background-color: violet;
+            float: left;">
+    <h3>Left Div</h3>
+ </div>
+ <div style="width: 85%;
+            height: 500px;
+            background-color: orange;
+            float: left;">
+    <h3>Right Div</h3>
+ </div>
+
+ <?php include "footer.php"; ?>
