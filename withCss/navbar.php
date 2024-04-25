f7870d514d54fac5e7b0d4c082714b792d9abb19
Zeus D. Elderfield <150475099+zeusschoolacc@users.noreply.github.com>
2024-04-25 09:07:57 +0800

    Add files via upload

---
+++ withCss/navbar.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="./navbar.css"/>
+
+<div class="navbar flex">
+    <h1>Test Logo Here</h1>
+    <ul class="buttons flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+    </ul>
+</div>
